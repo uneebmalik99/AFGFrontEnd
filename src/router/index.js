4f943844ea0d38a769df4194510372b4a0be2527
Rao Haroon <raoharoon007@gmail.com>
2024-05-28 17:50:58 +0500

    New Changes
    1-Create About page
    2-changes in About us page
    3-Changes in Contact us page
    4-changes in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/pages/Home.vue';
 import ContactUs from '@/pages/ContactUs.vue';
-
+import About from '@/pages/About.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/contact-us', component: ContactUs },
-  
+  { path: '/about', component: About },
 ];
 
 const router = createRouter({
