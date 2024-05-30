543d0edf071888d0778a71c2f27acd9760e7e713
Rao Haroon <raoharoon007@gmail.com>
2024-05-30 18:11:29 +0500

    New Changes
    1-add 8 services page.

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,13 @@ import Home from '@/pages/Home.vue';
 import ContactUs from '@/pages/ContactUs.vue';
 import About from '@/pages/About.vue';
 import Tracking from '@/pages/Tracking.vue';
+import Services from '@/pages/Services.vue';
 const routes = [
   { path: '/', component: Home },
   { path: '/contact-us', component: ContactUs },
   { path: '/about', component: About },
   { path: '/tracking', component: Tracking },
+  { path: '/services', component: Services },
 ];
 
 const router = createRouter({
